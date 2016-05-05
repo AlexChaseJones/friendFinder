658075acba59ee23cf8bb1a13dc5303595e2a9ed
Alexander <alexchasejones@icloud.com>
2016-05-05 18:02:05 -0400

    bug fix attempt

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 // Sets up routes
-app.use('/', apiRouter);
 app.use('/', htmlRouter);
+app.use('/', apiRouter);
 
 app.listen(PORT, function(){
 	console.log('App listening on PORT ' + PORT);
